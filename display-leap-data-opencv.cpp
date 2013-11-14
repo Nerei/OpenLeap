87a7aad560c0e712d6cb9ec5d410d314a804f928
emccann <emccann@cs.uml.edu>
2013-11-14 15:44:34 -0500

    Make display-leap-data-opencv display the left and right cameras' data side-by-side

--- display-leap-data-opencv.cpp
+++ display-leap-data-opencv.cpp
@@ -8,10 +8,24 @@
 #include <highgui.h>
 #include <map>
 #include <queue>
+#include <stack>
 
 using namespace std;
 
 #define NUM_BUFFERS 2
+
+#define getIndex(x,y,ch) 3 * (x + 2 * VFRAME_WIDTH * y) + ch
+
+#define setChannel(x,y,ch,co) f->frame->imageData[getIndex(x,y,ch)] = (unsigned char)co;
+
+#define setPixel(x,y,cr,cg,cb) \
+    setChannel(x,y,0,cb) \
+    setChannel(x,y,1,cg) \
+    setChannel(x,y,2,cr)
+
+#define setTwoPixels(x,y,cr,cg,cb) \
+    setPixel(x,2*y,cr,cg,cb) \
+    setPixel(x,2*y+1,cr,cg,cb)
 
 typedef struct ctx_s ctx_t;
 struct ctx_s
@@ -32,8 +46,10 @@ ctx_t ctx_data;
 ctx_t *ctx = NULL;
 map<uint32_t, frame_t*> frame;
 vector<uint32_t> pending;
+stack<frame_t *> recycling;
 frame_t *current = NULL;
-uint32_t oldest = 0;
+
+const CvSize cvs = cvSize(VFRAME_WIDTH * 2, 2 * VFRAME_HEIGHT);
 
 void process_video_frame(ctx_t *ctx)
 {
@@ -47,7 +63,7 @@ void process_video_frame(ctx_t *ctx)
 
 void process_usb_frame(ctx_t *ctx, unsigned char *data, int size)
 {
-  int i;
+  int i,x,y;
 
   int bHeaderLen = data[0];
   int bmHeaderInfo = data[1];
@@ -55,6 +71,7 @@ void process_usb_frame(ctx_t *ctx, unsigned char *data, int size)
   uint32_t dwPresentationTime = *( (uint32_t *) &data[2] );
 
   frame_t* f = NULL;
+
   for(vector<uint32_t>::const_iterator it = pending.begin(); it!=pending.end();it++)
 	  if ((uint32_t)(*it) == dwPresentationTime)
 	  {
@@ -63,60 +80,48 @@ void process_usb_frame(ctx_t *ctx, unsigned char *data, int size)
 	  }
   if (f == NULL)
   {
-	  f = (frame_t *)malloc(sizeof(frame_t));
-	  memset(f, 0, sizeof(frame_t));
-	  f->frame = cvCreateImage( cvSize(VFRAME_WIDTH, 2 * VFRAME_HEIGHT), IPL_DEPTH_8U, 3);
-	  frame[dwPresentationTime] = f;
-	  pending.push_back(dwPresentationTime);
-	  sort(pending.begin(),pending.end());	  
+      if (!recycling.empty()) {
+	    f = recycling.top();
+	    recycling.pop();
+        f->data_len = 0;
+        f->state = 0;
+      } else {
+	    f = (frame_t *)malloc(sizeof(frame_t));
+	    memset(f, 0, sizeof(frame_t));
+        f->frame = cvCreateImage(cvs, IPL_DEPTH_8U, 3);
+      }
+      frame[dwPresentationTime] = f;
+      pending.push_back(dwPresentationTime);
+      sort(pending.begin(),pending.end());
       f->id = dwPresentationTime;
   }
   
   //printf("frame time: %u\n", dwPresentationTime);
 
-  for (i = bHeaderLen; i < size ; i += 2) {
+  for (x=0,y=0,i=bHeaderLen; i < size; i += 2) {
     if (f->data_len >= VFRAME_SIZE)
     {
-      cvReleaseImage(&f->frame);
-      frame.erase(f->id);
-      pending.erase(remove(pending.begin(), pending.end(), f->id), pending.end());
-      free(f);
-      break ;
+      break;
     }
-
-    CvScalar s;
-    s.val[2] = data[i];
-    s.val[1] = data[i+1];
-    s.val[0] = 0;
-    int x = f->data_len % VFRAME_WIDTH;
-    int y = (int)floor((1.0f * f->data_len) / (1.0f * VFRAME_WIDTH));
-    cvSet2D(f->frame, 2 * y,     x, s);
-    cvSet2D(f->frame, 2 * y + 1, x, s);
+    x = f->data_len % VFRAME_WIDTH;
+    y = (int)floor((1.0f * f->data_len) / (1.0f * VFRAME_WIDTH));
+    setTwoPixels(x,y,0,data[i],data[i])
+    setTwoPixels(x+VFRAME_WIDTH,y,data[i+1],0,0)
     f->data_len++;
   }
 
-  if (dwPresentationTime != f->id && f->id > 0) {
-    printf("mixed frame TS: (id=%i, dwPresentationTime=%i) -- dropping frame\n", f->id, dwPresentationTime);
-    cvReleaseImage(&f->frame);
-    frame.erase(f->id);
-    pending.erase(remove(pending.begin(), pending.end(), f->id), pending.end());
-    free(f);
-    return ;
-  }
   if (bmHeaderInfo & UVC_STREAM_EOF) {
     //printf("End-of-Frame.  Got %i\n", f->data_len);
     if (f->data_len != VFRAME_SIZE) {
       printf("wrong frame size got %i expected %i\n", f->data_len, VFRAME_SIZE);
-      cvReleaseImage(&f->frame);
+      recycling.push(f);
       frame.erase(f->id);
       pending.erase(remove(pending.begin(), pending.end(), f->id), pending.end());
-      free(f);
       return ;
     }
 
     if (current!=NULL) {
-    	cvReleaseImage(&current->frame);
-    	free(current);
+      recycling.push(current);
     }
     current = frame[dwPresentationTime];
     pending.erase(remove(pending.begin(), pending.end(), dwPresentationTime), pending.end());
@@ -124,13 +129,14 @@ void process_usb_frame(ctx_t *ctx, unsigned char *data, int size)
     sort(pending.begin(),pending.end());
     if (pending.size() > 10) {
         printf("Oh noez! There are %d pending frames in the queue!\n",(int)pending.size());
-/*        for(vector<uint32_t>::const_iterator it = pending.begin(); it!=pending.begin()+5;it++)
+        for(vector<uint32_t>::const_iterator it = pending.begin(); it!=pending.begin()+5;it++)
         {
-            cvReleaseImage(&frame[*it]->frame);
-            free(frame[*it]);
-            frame.erase(*it);
+          recycling.push(frame[*it]);
+          frame.erase(*it);
         }
-        pending.erase(pending.begin(), pending.begin()+5);*/
+        reverse(pending.begin(), pending.end());
+        pending.resize(5);
+        reverse(pending.begin(), pending.end());
     }
 
     process_video_frame(ctx);
@@ -147,7 +153,7 @@ int main(int argc, char *argv[])
   memset(&ctx_data, 0, sizeof (ctx_data));
   ctx = &ctx_data;
   cvNamedWindow("mainWin", 0);
-  cvResizeWindow("mainWin", VFRAME_WIDTH, VFRAME_HEIGHT * 2);
+  cvResizeWindow("mainWin", 2 * VFRAME_WIDTH, 2 * VFRAME_HEIGHT);
   init();
   setDataCallback(&gotData);
   spin();
@@ -162,6 +168,12 @@ int main(int argc, char *argv[])
 	cvReleaseImage(&tmp->frame);
 	frame.erase((uint32_t)*it);
 	free(tmp);
+  }
+  while(!recycling.empty()) {
+    frame_t *tmp = recycling.top();
+    recycling.pop();
+    cvReleaseImage(&tmp->frame);
+    free(tmp);
   }
 
   return (0);
